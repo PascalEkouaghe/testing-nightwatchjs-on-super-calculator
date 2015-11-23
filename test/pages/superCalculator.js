d6620f2df30fd6fb0235f4b917bb1ee48badf89b
ekougs <ekougs@gmail.com>
2015-11-23 15:14:37 +0100

    Using page objects to write more functional tests, cleaning

--- test/pages/superCalculator.js
+++ test/pages/superCalculator.js
@@ -1,10 +1,19 @@
-module.exports = {
-  url:'http://juliemr.github.io/protractor-demo/',
-  elements: {
-    firstOp: 'input[ng-model=first]',
-    secondOp: 'input[ng-model=second]',
-    calculate: '#gobutton',
-    result: 'h2.ng-binding',
-    operator: 'select[ng-model=operator]'
+(function() {
+  var superCalculatorCommands = {
+    calculate: function() {
+      this.click('@calculate').api.pause(2500);
+      return this;
+    }
+  };
+  module.exports = {
+    commands: [superCalculatorCommands],
+    url:'http://juliemr.github.io/protractor-demo/',
+    elements: {
+      firstOp: 'input[ng-model=first]',
+      secondOp: 'input[ng-model=second]',
+      calculate: '#gobutton',
+      result: 'h2.ng-binding',
+      operator: 'select[ng-model=operator]'
+    }
   }
-}
+})();
--- test/specs/AdditionSpecs.js
+++ test/specs/AdditionSpecs.js
@@ -1,29 +1,29 @@
-module.exports = {
-  before : function(client) {
-    console.log('Setting up...');
-    this.superCalculator = client.page.superCalculator();
-    this.superCalculator.navigate();
-  },
+(function() {
+  module.exports = {
+    before : function(client) {
+      console.log('Setting up...');
+      this.superCalculator = client.page.superCalculator();
+      this.superCalculator.navigate();
+    },
 
-  after : function(client) {
-    client.end();
-    console.log('Closing down...');
-  },
+    after : function(client) {
+      client.end();
+      console.log('Closing down...');
+    },
 
-  '1 + 2 = 3': function(client) {
-    this.superCalculator.setValue('@firstOp', 1)
-    .setValue('@secondOp', 2)
-    .click('@calculate')
-    client.pause(2500);
-    this.superCalculator.assert.containsText('@result', '3');
-  },
+    '1 + 2 = 3': function(client) {
+      this.superCalculator.setValue('@firstOp', 1)
+      .setValue('@secondOp', 2)
+      .calculate()
+      .assert.containsText('@result', '3');
+    },
 
-  '3 - 1 = 2': function(client) {
-    this.superCalculator.setValue('@firstOp', 3)
-    .setValue('@secondOp', 1)
-    .setValue('@operator', '-')
-    .click('@calculate');
-    client.pause(2500);
-    this.superCalculator.assert.containsText('@result', '2');
+    '3 - 1 = 2': function(client) {
+      this.superCalculator.setValue('@firstOp', 3)
+      .setValue('@secondOp', 1)
+      .setValue('@operator', '-')
+      .calculate()
+      .assert.containsText('@result', '2');
+    }
   }
-}
+})();
